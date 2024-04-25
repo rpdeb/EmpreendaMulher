bc87187e8d32c9da7cf9dfe267f7a507fcc87d8b
rpdeb <deboraribeiro9015@gmail.com>
2024-04-25 03:04:46 -0300

    Implementa Componente NavBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RodapeComponent } from './rodape/rodape.component';
+import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    RodapeComponent
+    RodapeComponent,
+    NavBarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'

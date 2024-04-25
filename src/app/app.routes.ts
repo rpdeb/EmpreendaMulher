4782d7747a04cf31c7ebd3e03c519bc9ad023b80
rpdeb <deboraribeiro9015@gmail.com>
2024-04-25 02:44:50 -0300

    Implementa Componente Galeria e Rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'path';
+import { GaleriaComponent } from './galeria/galeria.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'galeria',component:GaleriaComponent} 
+];
---
+++ src/app/galeria/galeria.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RodapeComponent } from '../rodape/rodape.component';
+
+@Component({
+  selector: 'app-galeria',
+  standalone: true,
+  imports: [RodapeComponent],
+  templateUrl: './galeria.component.html',
+  styleUrl: './galeria.component.css'
+})
+export class GaleriaComponent {
+
+}

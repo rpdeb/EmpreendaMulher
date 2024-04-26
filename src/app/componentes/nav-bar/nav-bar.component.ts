ffb4f21bea9d1abdfdb984176b02304f9ec4e697
rpdeb <deboraribeiro9015@gmail.com>
2024-04-26 10:32:57 -0300

    Inserção de Cards

--- src/app/componentes/nav-bar/nav-bar.component.ts
+++ src/app/componentes/nav-bar/nav-bar.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [MatMenuModule, MatButtonModule,
+    MatIconModule, MatToolbarModule
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 @Component({
   selector: 'app-galeria',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule,
+    MatIconModule, MatToolbarModule
+  ],
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.css'
 })
